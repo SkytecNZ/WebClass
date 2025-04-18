5ca2050f234b3006053473e196b09b2b874477ae
SkytecNZ <nznilan@gmail.com>
2025-04-19 10:03:17 +1200

    Header hanged to Red colour

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000
  
 app.get("/", (req, res, next) => {
     console.log("I received a get request on the path // ")
-    res.send('<h1 style="color:blue;">Acknowledged</h1>')
+    res.send('<h1 style="color:red;">Acknowledged</h1>')
 })
  
 
