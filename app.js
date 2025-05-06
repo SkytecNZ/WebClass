6371a1abd72aec175f46f88396059d0a180ce40c
SkytecNZ <nznilan@gmail.com>
2025-05-06 17:53:43 +1200

    Updated app.js as per Aziz's doc

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 console.log("Application is starting");
  
-const express = require('express')
+const express = require('express') // Importing express library
 const ejs = require('ejs')
-const app = express()
+const app = express() // Creating an instance of express application
 const port = 3000
  
 app.get("/", (req, res, next) => {
     console.log("I received a get request on the path // ")
-    res.send('<h1 style="color:red;">Acknowledged</h1>')
+    res.send('<h2 style="color:blue;">This is the app root</h2>')
 })
- 
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
- 
+  
+//Start server and listen on port 3000
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`) // Log a message when the server starts
 })
